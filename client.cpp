70300cbbb5ff0698a0b9fbcbf9123028f9f5759e
Ninghao Miao <ninghao.miao.2002@gmail.com>
2024-07-20 21:54:40 +0100

    first upload client and server cpp files

---
+++ client.cpp
@@ -0,0 +1,108 @@
+﻿#include <stdio.h>
+#include <winSock2.h>
+#pragma comment(lib,"ws2_32.lib")
+//link to winsocket library
+#include <graphics.h>
+//link to a library that can give us access for a messages record window, we will set the window later in the main function
+SOCKET sSocket;
+#pragma warning(disable:4996) 
+//this code is just for avoiding the errors in socket2 functions
+
+DWORD WINAPI scanfAndsend(LPVOID a);
+//function prototype in C++ in front of the main function
+
+int main() {
+	initgraph(400, 600, 1);
+	//initialize the height and width of the windows for clients
+
+	//1.set up the protocol version
+	WSADATA wsaData; 
+	//The WSADATA structure contains information about the Windows Sockets implementation.
+    //https://learn.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-wsadata 
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	//initialize the data definition language that the winsocket process used
+	//set up the prootocal version here, the protocal version should be 2.2
+	//MAKEWORD builds a 16 bits words from two 1 bytes word
+	
+	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+		printf("WinSocket protocol version is incorrect!\n");
+		//9.clear the protocol info if the requested protocol version is not 2.2
+		WSACleanup();
+		return -1;
+	}
+	printf("WinSocket protocol version is 2.2!\n");
+	//2.creat the socket
+	sSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	//AF_INET: IPv4 address family 
+	//SOCK_STREAM: for TCP
+	//IPPROTO_TCP: TCP
+	if (SOCKET_ERROR == sSocket) {
+	//in case any errors happened, then print out failed to creat socket
+		printf("Failed to create socket:%d\n", GetLastError());
+		return -2;
+	}
+	printf("Created Socket Successfully!\n");
+	//3.set up the server protocol address family
+	SOCKADDR_IN addr = { 0 };	
+	addr.sin_family = AF_INET;
+	//AF_INET: IPv4 address family, the same with the created socket before
+	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	//local ip address which can test in different devices easily
+	addr.sin_port = htons(9671);
+	//server port number
+	//4.connect to the server
+	int r = connect(sSocket, (sockaddr*)&addr, sizeof addr);
+	if (-1 == r) {
+		printf("Failed to connect to server:%d\n", GetLastError());
+		return -2;
+	}
+	printf("Connected to Server Successfully \n");
+
+	CreateThread(NULL, NULL, scanfAndsend, NULL, NULL, NULL);
+
+	char buff[128];
+	int n = 0;
+	while (1) {
+		r = recv(sSocket, buff, 127, NULL);
+		//infinite loop to receive data
+		if (r > 0) {
+			buff[r] = 0;
+			//when we meet the 0, it means the message ends
+			outtextxy(1, n * 20, buff);
+			//output the strings to chat room window
+			n++;
+			if (n > 29) {
+				n = 0;
+			//because each line has a 20 unit height and the window's height is 600. So the maximum messages could be 29 lines
+				cleardevice();
+			}
+		}
+	}
+	return 0;
+}
+
+DWORD WINAPI scanfAndsend(LPVOID a) {
+	//5.communicate
+	char buff[128];
+	int index = (int) a;
+	//detect how many of the clients connected to the server
+	while (1) {
+		memset(buff, 0, 128);
+		//clear temp for each loop
+		printf("You are Client %d, write something to the chat room: ",index+1);
+		//because the lpParam starts from 0 so normally the first people we call it client 1 so the number should be index+1
+		//scanf("%", buff);
+		scanf("%[^\n]", buff);
+		//scan the messages we wrote
+		char a = getchar();
+		//read the \n into the getchar in case scanf will jump directly for the next loop
+		send(sSocket, buff, strlen(buff), NULL);//send the messages to the server
+	}
+
+
+
+}
+
+
+
+
---
+++ server.cpp
@@ -0,0 +1,135 @@
+﻿#include <stdio.h>
+#include <winSock2.h>
+#pragma comment(lib,"ws2_32.lib")
+//link to winsocket library
+#include <windows.h>
+#pragma warning(disable:4996) 
+//this code is just for avoiding the errors in socket2 functions
+//above are basic steps for Initialising Winsock
+//https://www.binarytides.com/winsock-socket-programming-tutorial/
+#define MaxClientNum 100
+//define the maxium number of the clients
+int count;
+//this count is used in the future for counting the number of clients
+
+SOCKET cSocket[MaxClientNum];
+//Define a SOCKET type array whose name is cSocket
+
+DWORD WINAPI communicate(LPVOID a);
+//function prototype in C++ in front of the main function
+//the thread function which can communicate with the clients
+
+int main() {
+	count = 0;
+	//1.set up the protocol version
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	//initialize the data definition language that the winsocket process used
+	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+	//check the version 
+		printf("The requested winSocket protocol version is not 2.2!\n");
+		
+		WSACleanup();
+        //9.clear the protocol info if the requested protocol version is not 2.2
+		return -1;
+	}
+	printf("The requested winSocket protocol version is 2.2!\n");
+	//2.creat socket
+	SOCKET sSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	//AF_INET: IPv4 address family 
+    //SOCK_STREAM: for TCP
+    //IPPROTO_TCP: TCP
+	if (SOCKET_ERROR == sSocket) {
+		printf("Failed to create socket:%d\n", GetLastError());
+		return -2;
+	}
+	printf("Successfully created socket\n");
+	//3.set up the server protocol address family
+	SOCKADDR_IN addr = { 0 };
+	addr.sin_family = AF_INET;
+	//AF_INET: IPv4 address family, the same with the created socket before
+	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	//local ip address which can test in different devices easily
+	addr.sin_port = htons(9671);
+	//server port number, bind with the client, could be any number(just make sure the port isn't used)
+	//4.bind
+	int r = bind(sSocket, (sockaddr*)&addr, sizeof addr);
+	if (-1 == r) {
+		printf("Failed to bind socket:%d\n", GetLastError());
+		//8.close the connection
+		closesocket(sSocket);
+		//9.clear the protocol info
+		WSACleanup();
+		return -2;
+	}
+	printf("Bind Socket Successfully!\n");
+	
+	//5.listen
+	r = listen(sSocket, SOMAXCONN);
+	if (-1 == r) {
+		printf("Failed to listen socket:%d\n", GetLastError());
+		//8.close the connection
+		closesocket(sSocket);
+		//9.clear the protocol info
+		WSACleanup();
+		return -2;
+	}
+	printf("Successfully listen to socket\n");
+	//6.wait for the clients for connection
+	
+	for(int i = 0;i < MaxClientNum; i++){
+	//i is the number of the client which has connected to the server
+		cSocket[i]=accept(sSocket, NULL, NULL);
+		//connect the server in the client
+		if (SOCKET_ERROR == cSocket[i]) {
+			printf("Server crushed:%d\n", GetLastError());
+			//8.close the connection
+			closesocket(sSocket);
+			//9.clear the protocol info
+			WSACleanup();
+			return -3;
+		}
+		printf("Client number %d has successfully connected to server\n",i+1);
+		count++;
+		//while each one client is added, the count should add one
+		//creat a thread communicate with client
+		CreateThread(NULL, NULL,//could be 0 or NULL
+			communicate,//lpStartAddress
+			(LPVOID)i,//type conversion
+			NULL,NULL);//could be 0 or NULL
+	}
+	while (1);
+	return 0;
+}
+
+
+//thread which communicate with client
+//the thread function which can communicate with the clients
+DWORD WINAPI communicate(LPVOID a) {
+	//7.communicate
+	int index = (int) a;
+	char buff[128];
+	int r;
+	char temp[128];
+	while (1) {
+		r = recv(cSocket[index], buff, 127, NULL);
+		//receive the date from the clients
+		if (r > 0) {
+			buff[r] = 0;
+			// terminate the strings
+			printf(">> Client %d: %s \n", index+1,buff);
+			//output the date we received
+			//at the same time send them to all the clients which have connected to the server
+			memset(temp, 0, 128);
+			//clear temp for each loop
+			sprintf(temp, "Client %d: %s", index+1, buff);
+			for (int i = 0; i < count; i++) {
+				send(cSocket[i], temp, strlen(temp),NULL);
+				//send them to all the clients
+			}
+		}
+	}
+	return 0;
+}
+
+
